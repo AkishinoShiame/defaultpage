f260f7bdccd0ea9b2368f9d7dfdb2f47cb0ef10b
秋篠詩雨 <11460852+AkishinoShiame@users.noreply.github.com>
2023-11-10 14:01:02 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>It works!</title>
+  </head>
+  <body>
+    <h2>This is it.</h2>
+    <h4><?php echo "Hostname: ".gethostname(); ?></h4>
+    <h5><?php echo date("Y-m-d h:i:sa");?></h5>
+  </body>
+</html>
